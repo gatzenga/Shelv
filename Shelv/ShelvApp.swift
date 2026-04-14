c4bc72b217e74e84248f085d488fe8ddeefd468e
GatzeStreicheln <login@gatze.ch>
2026-04-14 18:15:45 +0200

    Initial Commit

---
+++ Shelv/ShelvApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelvApp.swift
+//  Shelv
+//
+//  Created by Vasco Kugler on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShelvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
