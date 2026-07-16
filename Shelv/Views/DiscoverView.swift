87f8d47e1952f5544358bf8b45567f6e0fd6e1ff
gatzenga <login@gatze.ch>
2026-07-16 21:49:17 +0200

    Use clock icons for latest downloads

--- Shelv/Views/DiscoverView.swift
+++ Shelv/Views/DiscoverView.swift
@@ -535,7 +535,7 @@ struct DiscoverView: View {
 
                 mixButton(
                     title: String(localized: "mix_latest_downloads"),
-                    icon: "arrow.down.circle.fill",
+                    icon: "clock.fill",
                     key: "offline_newest"
                 ) { loadOfflineMix(type: "offline_newest") }
             }
